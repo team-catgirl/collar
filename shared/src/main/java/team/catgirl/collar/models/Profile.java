35defd6e766fe3dd8d36f6fc6cbf21f8cdbf1be7
orsond <orsondmc@gmail.com>
2021-01-24 16:25:17 +1100

    add favouriteColor

--- shared/src/main/java/team/catgirl/collar/models/Profile.java
+++ shared/src/main/java/team/catgirl/collar/models/Profile.java
@@ -6,12 +6,14 @@ public class Profile {
     public final Friends friends;
     public final Byte[] capeImage;
     public final Byte[] profileImage;
+    public final String favouriteColor;
 
-    public Profile(String id, String displayName, Friends friends, Byte[] capeImage, Byte[] profileImage) {
+    public Profile(String id, String displayName, Friends friends, Byte[] capeImage, Byte[] profileImage, String favouriteColor) {
         this.id = id;
         this.displayName = displayName;
         this.friends = friends;
         this.capeImage = capeImage;
         this.profileImage = profileImage;
+        this.favouriteColor = favouriteColor;
     }
 }
--- shared/src/main/java/team/catgirl/collar/models/ProfileInfo.java
+++ shared/src/main/java/team/catgirl/collar/models/ProfileInfo.java
@@ -8,15 +8,17 @@ public class ProfileInfo {
     public final String displayName;
     public final Byte[] capeImage;
     public final Byte[] profileImage;
+    public final String favouriteColor;
 
-    public ProfileInfo(String id, String displayName, Byte[] capeImage, Byte[] profileImage) {
+    public ProfileInfo(String id, String displayName, Byte[] capeImage, Byte[] profileImage, String favouriteColor) {
         this.id = id;
         this.displayName = displayName;
         this.capeImage = capeImage;
         this.profileImage = profileImage;
+        this.favouriteColor = favouriteColor;
     }
 
     public static ProfileInfo from(Profile profile) {
-        return new ProfileInfo(profile.id, profile.displayName, profile.capeImage, profile.profileImage);
+        return new ProfileInfo(profile.id, profile.displayName, profile.capeImage, profile.profileImage, profile.favouriteColor);
     }
 }

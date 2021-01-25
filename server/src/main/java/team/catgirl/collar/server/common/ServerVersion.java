a0a545a1570ad4962e8a27b2c015532b2889c818
orsond <orsondmc@gmail.com>
2021-01-25 11:56:48 +1100

    FIX: buildpack breaks because it can't query the git repo during the build???

--- server/src/main/java/team/catgirl/collar/server/common/ServerVersion.java
+++ server/src/main/java/team/catgirl/collar/server/common/ServerVersion.java
@@ -1,29 +1,15 @@
 package team.catgirl.collar.server.common;
 
-import com.google.common.io.Resources;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Properties;
 
 public final class ServerVersion {
     public final String version;
-    public final String builtAt;
 
-    public ServerVersion(String version, String builtAt) {
+    public ServerVersion(String version) {
         this.version = version;
-        this.builtAt = builtAt;
     }
 
     public static ServerVersion version() throws IOException {
-        URL resource = Resources.getResource("version.properties");
-        Properties properties = new Properties();
-        try (InputStream is = resource.openStream()) {
-            properties.load(is);
-            String version = properties.getProperty("git.commit.id");
-            String builtAt = properties.getProperty("git.build.time");
-            return new ServerVersion(version, builtAt);
-        }
+        return new ServerVersion("1.0-beta");
     }
 }

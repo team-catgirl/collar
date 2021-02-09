877ff006cb9907a52f442b20cfae98e5dae30a13
orsond <orsondmc@gmail.com>
2021-02-09 12:58:25 +1100

    Use a fresh database every time we use the CollarServerRule

--- tests/src/main/java/team/catgirl/collar/tests/junit/CollarServerRule.java
+++ tests/src/main/java/team/catgirl/collar/tests/junit/CollarServerRule.java
@@ -1,5 +1,6 @@
 package team.catgirl.collar.tests.junit;
 
+import com.mongodb.client.MongoDatabase;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -7,6 +8,7 @@ import spark.Spark;
 import team.catgirl.collar.server.Main;
 import team.catgirl.collar.server.Services;
 import team.catgirl.collar.server.configuration.Configuration;
+import team.catgirl.collar.server.mongo.Mongo;
 
 import java.io.IOException;
 import java.net.URI;
@@ -20,7 +22,7 @@ import java.util.function.Consumer;
 public final class CollarServerRule implements TestRule {
 
     private final Consumer<Services> setupState;
-    public Main.Server server = new Main.Server(Configuration.testConfiguration());
+    public Main.Server server;
 
     public CollarServerRule(Consumer<Services> setupState) {
         this.setupState = setupState;
@@ -28,6 +30,9 @@ public final class CollarServerRule implements TestRule {
 
     @Override
     public Statement apply(Statement base, Description description) {
+        Mongo.getTestingDatabase().drop();
+        MongoDatabase db = Mongo.getTestingDatabase();
+        server = new Main.Server(Configuration.testConfiguration(db));
         return new Statement() {
             @Override public void evaluate() throws Throwable {
                 server.start(setupState);
@@ -35,6 +40,7 @@ public final class CollarServerRule implements TestRule {
                     base.evaluate();
                 } finally {
                     Spark.stop();
+                    db.drop();
                 }
             }
         };

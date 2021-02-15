08a16fcc60b9fedc4bbada7ebb66040d14b05a5a
orsond <orsondmc@gmail.com>
2021-02-15 20:37:45 +1100

    adding annotations

--- shared/src/main/java/team/catgirl/collar/protocol/messaging/SendMessageResponse.java
+++ shared/src/main/java/team/catgirl/collar/protocol/messaging/SendMessageResponse.java
@@ -1,17 +1,20 @@
 package team.catgirl.collar.protocol.messaging;
 
-import team.catgirl.collar.api.messaging.Message;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import team.catgirl.collar.protocol.ProtocolResponse;
 import team.catgirl.collar.security.ClientIdentity;
 import team.catgirl.collar.security.ServerIdentity;
 import team.catgirl.collar.security.mojang.MinecraftPlayer;
 
-public class SendMessageResponse extends ProtocolResponse {
+public final class SendMessageResponse extends ProtocolResponse {
     public final ClientIdentity sender;
     public final MinecraftPlayer player;
     public final byte[] message;
 
-    public SendMessageResponse(ServerIdentity identity, ClientIdentity sender, MinecraftPlayer player, byte[] message) {
+    public SendMessageResponse(@JsonProperty("identity") ServerIdentity identity,
+                               @JsonProperty("sender") ClientIdentity sender,
+                               @JsonProperty("player") MinecraftPlayer player,
+                               @JsonProperty("message") byte[] message) {
         super(identity);
         this.sender = sender;
         this.player = player;

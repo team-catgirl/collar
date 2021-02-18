6ea9f34a235e8fcca7e17cd485d027a2f8665222
orsond <orsondmc@gmail.com>
2021-02-18 11:09:17 +1100

    Private messaging works

--- shared/src/main/java/team/catgirl/collar/api/messaging/TextMessage.java
+++ shared/src/main/java/team/catgirl/collar/api/messaging/TextMessage.java
@@ -6,10 +6,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * A simple text message
  */
 public final class TextMessage implements Message {
+
+    public static final int MAX_LENGTH = 2000;
+
     @JsonProperty("content")
     public final String content;
 
-    public TextMessage(String content) {
+    public TextMessage(@JsonProperty("content") String content) {
+        if (content.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("content exceeds " + MAX_LENGTH + " characters");
+        }
         this.content = content;
     }
 }

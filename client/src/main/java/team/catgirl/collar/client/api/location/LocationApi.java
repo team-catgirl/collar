d5d9cdc3bbc6ac7ada966d1649c9fbf1a767e2ad
orsond <orsondmc@gmail.com>
2021-02-22 12:13:03 +1100

    updates

--- client/src/main/java/team/catgirl/collar/client/api/location/LocationApi.java
+++ client/src/main/java/team/catgirl/collar/client/api/location/LocationApi.java
@@ -121,7 +121,7 @@ public class LocationApi extends AbstractApi<LocationListener> {
 
     void publishNearby(Set<Entity> entities) {
         Set<String> nearbyHashes = entities.stream().filter(entity -> entity.type.equals(EntityType.PLAYER))
-                .limit(500)
+                .limit(200)
                 .map(entity -> Hashing.sha256().hashString(entity.id.toString(), StandardCharsets.UTF_8).toString())
                 .collect(Collectors.toSet());
         sender.accept(new UpdateNearbyRequest(identity(), nearbyHashes));

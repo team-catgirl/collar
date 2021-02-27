1c1974b2668d602aca261ccbdd908f6b43d52a63
orsond <orsondmc@gmail.com>
2021-02-27 13:58:43 +1100

    cleanup imports

--- client/src/main/java/team/catgirl/collar/client/security/PrivateIdentity.java
+++ client/src/main/java/team/catgirl/collar/client/security/PrivateIdentity.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.function.Consumer;
 
 /**
  * Identity used for storing and reading personal encrypted data on the server
